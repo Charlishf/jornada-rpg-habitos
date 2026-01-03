2ff8472ac5b537b452a9623c4dedf3b98f6c85a7
Charlishf <135128862+Charlishf@users.noreply.github.com>
2026-01-03 15:09:29 -0300

    Inserir dados supabase

--- supabase.ts
+++ supabase.ts
@@ -1,7 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.https://jyfrnpratsoalwmfcnax.supabase.co;
+const supabaseAnonKey = import.meta.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5ZnJucHJhdHNvYWx3bWZjbmF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY1MDg5MjgsImV4cCI6MjA4MjA4NDkyOH0.bpbGSaHZE7MtBhMBQ4xiP2CEZy7PCjx5gNmbzLLkbe8;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Variáveis de ambiente do Supabase não configuradas');
+}
 
 export const supabase = createClient(
   supabaseUrl,
